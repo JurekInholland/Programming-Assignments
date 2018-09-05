416d249146b45956968ec9e7024a8b805b269ec5
JurekInholland <jurekbaumann@me.com>
2018-09-05 14:22:37 +0200

    First exercise

---
+++ IntroductionPP1/Exercise3/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Exercise3
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            int score = 200;
+            int level = 3;
+            int nItems = 4;
+
+            int score_new, level_new;
+
+            // Increase score and level
+            score_new = score++;
+
+            level_new = level++;
+
+            // Increase nItems by 5
+            nItems += 5;
+
+            // Output new values
+            Console.WriteLine("Score (old): {0}, score (new): {1}", score_new, score);
+
+            Console.WriteLine("level (old): {0}, level (new): {1}", level_new, level);
+
+            Console.WriteLine("number of items: {0}", nItems);
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ IntroductionPP1/Exercise4/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Exercise4
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+
+            Console.WriteLine("Please tell me your age.");
+
+            string age;
+            age = Console.ReadLine();
+
+            const int tooOld = 65;
+
+            if (int.Parse(age) > tooOld) {
+                Console.WriteLine("You are too old.");
+            }
+
+            else {
+                Console.WriteLine("Fortunately, you are not too old yet.");
+            }
+
+        }
+    }
+}
