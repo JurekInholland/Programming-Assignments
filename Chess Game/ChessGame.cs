c79042423ca15cc247d97797691a3c6ef258e4eb
JurekInholland <jurekbaumann@me.com>
2019-01-08 22:08:59 +0100

    Add project files.

---
+++ Chess Game/ChessGame.cs
@@ -0,0 +1,163 @@
+﻿using System;
+
+namespace Chess_Game
+{
+    class ChessGame
+    {
+        public ChessPiece[,] chessboard = new ChessPiece[8, 8];
+
+
+        // Init chessboard in the class constructor; Ask if this is considered best practice.
+        public ChessGame()
+        {
+            InitChessboard(chessboard);
+        }
+
+
+        void InitChessboard(ChessPiece[,] chessboard)
+        {
+            for (int row = 0; row < chessboard.GetLength(0); row++)
+            {
+                for (int col = 0; col < chessboard.GetLength(1); col++)
+                {
+                    chessboard[row, col] = null;
+                }
+            }
+
+            PutChessPieces(chessboard);
+        }
+
+        void PutChessPieces(ChessPiece[,] chessboard)
+        {
+            ChessPieceType[] order = { ChessPieceType.Rook, ChessPieceType.Knight, ChessPieceType.Bishop, ChessPieceType.King, ChessPieceType.Queen, ChessPieceType.Bishop, ChessPieceType.Knight, ChessPieceType.Rook };
+
+            for (int row = 0; row < chessboard.GetLength(0); row++)
+            {
+                for (int col = 0; col < chessboard.GetLength(1); col++)
+                {
+                    // Set type
+                    if (row == 1 || row == 6)
+                    {
+                        chessboard[row, col] = new ChessPiece();
+                        chessboard[row, col].type = ChessPieceType.Pawn;
+
+                    }
+                    else if (row == 0 || row == 7)
+                    {
+
+                        chessboard[row, col] = new ChessPiece();
+
+                        chessboard[row, col].type = order[col];
+                    }
+
+                    // Set color
+                    if (row == 0 || row == 1)
+                    {
+                        chessboard[row, col].color = ChessPieceColor.White;
+                    }
+
+                    else if (row == 6 || row == 7)
+                    {
+                        chessboard[row, col].color = ChessPieceColor.Black;
+                    }
+                }
+            }
+        }
+
+        public void CheckMove(ChessPiece[,] chessboard, Position from, Position to)
+        {
+            if (chessboard[from.field, from.column] == null)
+            {
+                Console.WriteLine("No chesspiece at from position!");
+                return;
+            }
+
+            if (chessboard[to.field, to.column] != null)
+            {
+                Console.WriteLine("There is a pice on the to position.");
+                return;
+            }
+
+            // check if valid
+
+            bool isValid = ValidMove(chessboard[from.field, from.column], from, to);
+            if (isValid)
+            {
+                DoMove(chessboard, from, to);
+            }
+            else
+            {
+                Console.WriteLine("Invalid move for chesspiece {0}", chessboard[from.field, from.column].type);
+            }
+        }
+
+        bool ValidMove(ChessPiece chessPiece, Position from, Position to)
+        {
+            int ver = Math.Abs(from.field - to.field);
+            int hor = Math.Abs(from.column - to.column);
+
+            if (hor == 0 && ver == 0)
+            {
+                return false;
+            }
+            else
+            {
+                switch (chessPiece.type)
+                {
+                    case ChessPieceType.Bishop:
+                        if (hor == ver)
+                        {
+                            return true;
+                        }
+                        break;
+
+                    case ChessPieceType.King:
+                        if (hor == 1 || ver == 1 || hor == 1 && ver == 1)
+                        {
+                            return true;
+                        }
+                        break;
+
+                    case ChessPieceType.Knight:
+                        if (hor * ver == 2)
+                        {
+                            return true;
+                        }
+                        break;
+
+                    case ChessPieceType.Pawn:
+                        if (hor == 0 && ver == 1)
+                        {
+                            return true;
+                        }
+                        break;
+
+                    case ChessPieceType.Queen:
+                        if (hor * ver == 0 || hor == ver)
+                        {
+                            return true;
+                        }
+                        break;
+
+                    case ChessPieceType.Rook:
+                        if (hor * ver == 0)
+                        {
+                            return true;
+                        }
+                        break;
+
+                }
+                return false;
+            }
+
+
+        }
+
+        void DoMove(ChessPiece[,] chessboard, Position from, Position to)
+        {
+            chessboard[to.field, to.column] = chessboard[from.field, from.column];
+            chessboard[from.field, from.column] = null;
+        }
+
+    }
+}
---
+++ Chess Game/Program.cs
@@ -0,0 +1,164 @@
+﻿using System;
+
+
+namespace Chess_Game
+{
+    class Program
+    {
+        // Ask for difference between instantiating here or within class constructor
+        ChessGame chessgame = new ChessGame();
+
+        static void Main()
+        {
+            Program myProgram = new Program();
+            myProgram.Start();
+        }
+
+        void Start()
+        {
+            DisplayChessboard(chessgame.chessboard);
+            PlayChess(chessgame.chessboard);
+        }
+
+       
+
+        void DisplayChessboard(ChessPiece[,] chessboard)
+        {
+
+            string[] colLetters = { "  ", "A ", "B ", "C ", "D ", "E ", "F ", "G ", "H " };
+
+            for (int row = 0; row < chessboard.GetLength(0) + 1; row++)
+            {
+                for (int col = 0; col < chessboard.GetLength(1) + 1; col++)
+                {
+                    if (row == 0)
+                    {
+                        Console.Write("{0} ", colLetters[col]);
+                        continue;
+                    }
+                    else if (col == 0)
+                    {
+                        Console.Write("{0} ", row);
+                        continue;
+                    }
+
+                    if ((row + col) % 2 == 0)
+                    {
+                        Console.BackgroundColor = ConsoleColor.DarkYellow;
+                    }
+                    else
+                    {
+                        Console.BackgroundColor = ConsoleColor.Gray;
+                    }
+
+                    DisplayChessPiece(chessboard[row - 1, col - 1]);
+
+                    Console.ResetColor();
+
+                }
+                Console.WriteLine();
+            }
+        }
+
+
+        
+        void DisplayChessPiece(ChessPiece chessPiece)
+        {
+            if (chessPiece == null)
+            {
+                Console.Write("   ");
+                return;
+            };
+            if (chessPiece.color == ChessPieceColor.Black)
+            {
+                Console.ForegroundColor = ConsoleColor.Black;
+            }
+            else
+            {
+                Console.ForegroundColor = ConsoleColor.White;
+            }
+
+            switch (chessPiece.type)
+            {
+                case ChessPieceType.Bishop:
+                    Console.Write(" b ");
+                    break;
+                case ChessPieceType.King:
+                    Console.Write(" K ");
+                    break;
+                case ChessPieceType.Knight:
+                    Console.Write(" k ");
+                    break;
+                case ChessPieceType.Pawn:
+                    Console.Write(" p ");
+                    break;
+                case ChessPieceType.Queen:
+                    Console.Write(" Q ");
+                    break;
+                case ChessPieceType.Rook:
+                    Console.Write(" r ");
+                    break;
+            }
+
+            Console.ResetColor();
+
+        }
+
+        Position ReadPosition(string question)
+        {
+            bool validPosition = false;
+            int row = 0;
+            int column = 0;
+
+            while (validPosition == false)
+            {
+                Console.Write(question);
+                string userPos = Console.ReadLine().ToUpper();
+
+                column = userPos[0] - 'A';
+                row = int.Parse(userPos[1].ToString()) - 1;
+
+                if (column <= 8 && row <= 8)
+                {
+                    validPosition = true;
+                }
+
+            }
+            Position playerPos = new Position();
+            playerPos.field = row;
+            playerPos.column = column;
+            return playerPos;
+
+        }
+
+        void PlayChess(ChessPiece[,] chessboard)
+        {
+            while (true)
+            {
+                try
+                {
+                    Position from = ReadPosition("Enter from posi: ");
+                    Position to = ReadPosition("Enter to posi: ");
+                    chessgame.CheckMove(chessboard, from, to);
+                }
+                catch
+                {
+                    Console.WriteLine("Please enter a valid position!");
+                }
+
+
+                // Check if move is valid
+
+                // Do move
+
+
+                DisplayChessboard(chessboard);
+
+            }
+        }
+
+        
+
+        
+    }
+}
